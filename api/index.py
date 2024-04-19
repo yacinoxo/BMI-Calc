c897b946c3f3613d95a2425bb6302928add042a6
yacinoc <137206550+yacinoxo@users.noreply.github.com>
2024-04-19 17:12:49 +0000

    push

--- api/index.py
+++ api/index.py
@@ -16,7 +16,8 @@ def Home():
         #cheke do we have the value of weight and height
         if height and weight :
             bmi = round(weight / ((height / 100)**2), 2)
-            return render_template('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="/static/styel.css"><title>BMI calculator</title></head><body><h1>BMI calculator</h1><br><p>Hi how is it going, I hope you are right.</p><form action="/" method="post"><label for="userheight">Height:</label><input type="text" name="userheight" placeholder="plz entre your Height in CM" required><br><br><label for="userweight">Weight:</label><input type="text" name="userweight" placeholder="plz entre your Weight in KG" required*><br><br><br><button type="submit">Calculate</button></form><br><div>{% if bmi %}<p>Your BMI is <i style="color: goldenrod">{{ bmi }}</i> <b style="color: red">kg/m²</b></p>{% endif %}</div></body></html>', bmi = bmi)
+            return render_template('
+            <!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="/static/styel.css"><title>BMI calculator</title></head><body><h1>BMI calculator</h1><br><p>Hi how is it going, I hope you are right.</p><form action="/" method="post"><label for="userheight">Height:</label><input type="text" name="userheight" placeholder="plz entre your Height in CM" required><br><br><label for="userweight">Weight:</label><input type="text" name="userweight" placeholder="plz entre your Weight in KG" required*><br><br><br><button type="submit">Calculate</button></form><br><div>{% if bmi %}<p>Your BMI is <i style="color: goldenrod">{{ bmi }}</i> <b style="color: red">kg/m²</b></p>{% endif %}</div></body></html>', bmi = bmi)
     return render_template("index.html")
     
 
